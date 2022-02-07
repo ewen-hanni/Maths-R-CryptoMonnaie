335c4fdf136950a3ebfc4cbbd1f8e37a559d5a4a
Ewen Hanni <48356811+ewen-hanni@users.noreply.github.com>
2022-02-07 20:53:12 +0100

    fix datetotimestamp

--- Crypto.R
+++ Crypto.R
@@ -12,16 +12,16 @@ nullToNA <- function(x) {
   x[sapply(x, is.null)] <- NA
   return(x)
 }
-is_date <- function(date, format) {
-  formatted = try(as.Date(date, format), silent = TRUE)
+is_date <- function(date) {
+  formatted = try(as.Date(date, "%d-%m-%Y"), silent = TRUE)
   return(DIZutils::equals2(as.character(formatted), date))
 }
 TimestamptoDate <- function(x) {
+  print(cat("TimestamptoDate 1 : " , x))
+   
+  x[sapply(x,is_date)] <- (as.POSIXct(as.numeric(as.character(x)), origin="1970-01-01", tz="GMT"))
   
-  #x[sapply(x, !(is.date))] <- (as.POSIXct(as.numeric(as.character(x)), origin="1970-01-01", tz="GMT"))
-  #x[sapply(x,is_date(x,"%d-%m-%Y"))] <- (as.POSIXct(as.numeric(as.character(x)), origin="1970-01-01", tz="GMT"))
-  x <- (as.POSIXct(as.numeric(as.character(x)), origin="1970-01-01", tz="GMT"))
-  
+  print(cat("TimestamptoDate 2 : " , x))
   return(x)
   #head(as.POSIXct(as.numeric(as.character(try$time)), origin="1970-01-01", tz="GMT"))
 }
@@ -34,7 +34,8 @@ head(timestamp)
 # Timestamp to date
 #mkt_data2$Date <- as.Date(as.POSIXct(timestamp, origin=“1970-01-01”))
 mkt_data <- lapply(mkt_data,nullToNA)
-#prends une demie éternité
+#prends une demie éternité :
+
 mkt_data$Date <- seq(lapply(mkt_data$Timestamp,TimestamptoDate))
 mkt_data <- do.call(rbind,lapply(mkt_data,data.frame,stringsAsFactors=FALSE))
 #head(mkt_data)
@@ -44,9 +45,10 @@ head(mkt_data$Date)
 # le tibble (≈petit df)
 bit_df = mkt_data %>%
   mutate(Date = dmy(Date)) %>%
-  mutate(Vol. = as.numeric(str_sub(Vol., end = -2))*1000,
-         Change = as.numeric(str_sub(Change, end = -2))) %>%
+  mutate(Vol. = as.numeric(str_sub(Volume_(Currency), end = -2))*1000,
+         Change = as.numeric(str_sub(Weighted_Price, end = -2))) %>%
   arrange(Date)
+
  #template des graph 
 PlotTemplate = theme(
                  plot.title = element_text(hjust = .5, size = 28, colour = 'yellow'),
@@ -60,8 +62,8 @@ PlotTemplate = theme(
                  strip.text = element_text(face = 'bold')) 
 
 # affichage graph
-ggplotly(ggplot(bit_df, aes(Date, Price)) + geom_line(col = 'orange') + 
+ggplotly(ggplot(bit_df, aes(Timestamp, Price)) + geom_line(col = 'orange') + 
            labs(title = 'Bitcoin', x = '') +
-           scale_y_continuous(breaks = c(0, 5000, 10000, 15000), 
-                              labels = c('$0', '$5,000', '$10,000', '$15,000')) + PlotTemplate)
+           scale_y_continuous(breaks = c(0, 5000, 10000, 15000, 20000), 
+                              labels = c('$0', '$5,000', '$10,000', '$15,000', '$20,000')) + PlotTemplate)
 
