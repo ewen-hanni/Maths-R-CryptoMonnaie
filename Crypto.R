5fea52fc8ec49f3cb6a64e792711170271508c89
IvoCostaCunha <ivo.costacunha@icloud.com>
2022-02-08 22:09:51 +0100

    added starting functions

--- Crypto.R
+++ Crypto.R
@@ -17,6 +17,7 @@ sprintf("wd = %s", getwd())
 # Do all OS affected operations here
 if(os == "unix") {
   # unix like OS
+  print("Using an unix like OS")
 
 } else if (os == "windows") {
   # windows OS
@@ -27,9 +28,11 @@ if(os == "unix") {
   print("Not a supported OS")
 }
 
+#------------------------------------------------------------------------------#
+
 # Change all NULL values to NA
 setNullToNA <- function(x) {
-  x[sapply(x, is.null)] <- NA
+  x[sapply(x, 0)] <- NA
   return(x)
 }
 
@@ -39,15 +42,169 @@ is_date <- function(date) {
   return(DIZutils::equals2(as.character(formatted), date))
 }
 
-data <- read.csv(file = "crypto-markets.csv")
-data <- lapply(data, setNullToNA)
+#------------------------------------------------------------------------------#
 
+data <- read.csv(file = "crypto-markets.csv")
+#data <- lapply(data, setNullToNA)
+data[data == 0] <- NA
+
+#------------------------------------------------------------------------------#
+
+# Get all the data from 2016-01-01 to 2018-01-01
 get2016to2018Data <- function() {
-  selectData = data[data$date > "2016-01-01" & data$date < "2018-01-01",]
-  return(selectData)
+  df <- data[data$date > "2016-01-01" & data$date < "2018-01-01",]
+  return(df)
 }
 
+get2016to2018NonBTCData <- function() {
+  df <- get2016to2018Data()
+  nonBTCData = df[df$name != "Bitcoin",]
+  return(nonBTCData)
+}
+
+get2016to2018BTCData <- function() {
+  df <- get2016to2018Data()
+  btcData <- df[df$name == "Bitcoin",]
+  return(btcData)
+}
+
+#------------------------------------------------------------------------------#
+
+get2018Data <- function() {
+  df <- data[data$date > "2016-01-01" & data$date < "2018-01-01",]
+  return(df)
+}
+
+get2018BTCData <- function() {
+  df <- get2018Data()
+  btcData <- df[df$name == "Bitcoin",]
+  return(btcData)
+}
+
+get2018NonBTCData <- function() {
+  df <- get2018Data()
+  nonBTCData = df[df$name != "Bitcoin",]
+  return(nonBTCData)
+}
+
+#------------------------------------------------------------------------------#
+                              # 2016 to 2018
+# Average minimum value of BTC during the 2016 to 2018 period
+print("Avg min BTC value")
+df <- get2016to2018BTCData()
+minAvgBTCValue <- mean(df$low, na.rm=T)
+minAvgBTCValue
+
+# Average maximum value of BTC during the 2016 to 2018 period
+print("Avg max BTC value")
+df <- get2016to2018BTCData()
+maxAvgBTCValue <- mean(df$high, na.rm=T)
+maxAvgBTCValue
+
+# Average BTC volume traded during 2016 to 2018 period
+print("Average BTC traded volume")
+df <- get2016to2018BTCData()
+avgBTCVol <- mean(df$volume)
+avgBTCVol
+
+# Sum of all volume traded including BTC and the rest during 2016 to 2018 period
+print("Sum of all volume traded")
+df <- get2016to2018Data()
+tradedAllVol = sum(df$volume, na.rm=T)
+tradedAllVol
+
+# Sum of all BTC volume traded during 2016 to 2018 period
+print("Sum of BTC volume traded")
+df <- get2016to2018BTCData()
+tradedBTCVol <- sum(df$volume)
+tradedBTCVol
+
+# Sum of all non BTC volume traded during 2016-2018 period
+print("Sum of non BTC volume traded")
+df <- get2016to2018NonBTCData()
+tradedNonBTCVol <- sum(df$volume, na.rm=T)
+tradedNonBTCVol
+
+# Percentage of BTC volume traded on all volume traded during 2016 to 2018 period
+print("Percentage of BTC volume traded on all volume")
+percentageBTC <- tradedBTCVol/tradedAllVol
+percentageBTC
+
+# Percentage of non BTC volume traded on all volume traded during 2016 to 2018 period
+print("Percentage of non BTC volume traded on all volume")
+percentageNonBTC <- tradedNonBTCVol/tradedAllVol
+percentageNonBTC
+
+#------------------------------------------------------------------------------#
+                              # 2018 ONLY
+
+# Average minimum value of BTC during the 2018 to 2019 period
+print("Avg min BTC value")
+df <- get2018BTCData()
+minAvgBTCValue <- mean(df$low, na.rm=T)
+minAvgBTCValue
+
+# Average maximum value of BTC during the 2018 to 2019 period
+print("Avg max BTC value")
+df <- get2018BTCData()
+maxAvgBTCValue <- mean(df$high, na.rm=T)
+maxAvgBTCValue
+
+# Average BTC volume traded during the 2018 to 2019 period
+print("Average BTC traded volume")
+df <- get2018BTCData()
+avgBTCVol <- mean(df$volume, na.rm=T)
+avgBTCVol
+
+# Sum of all volume traded including BTC and the rest during 2018 to 2019 period
+print("Sum of all volume traded")
+df <- get2018Data()
+tradedAllVol = sum(df$volume, na.rm=T)
+tradedAllVol
+
+# Sum of all BTC volume traded during 2018 to 2019 period
+print("Sum of BTC volume traded")
+df <- get2018BTCData()
+tradedBTCVol <- sum(df$volume, na.rm=T)
+tradedBTCVol
+
+# Sum of all non BTC volume traded during 2018-2019 period
+print("Sum of non BTC volume traded")
+df <- get2018NonBTCData()
+tradedNonBTCVol <- sum(df$volume, na.rm=T)
+tradedNonBTCVol
+
+# Percentage of BTC volume traded on all volume traded during 2018 to 2019 period
+print("Percentage of BTC volume traded on all volume")
+percentageBTC <- tradedBTCVol/tradedAllVol
+percentageBTC
+
+# Percentage of non BTC volume traded on all volume traded during 2016 to 2018 period
+print("Percentage of non BTC volume traded on all volume")
+percentageNonBTC <- tradedNonBTCVol/tradedAllVol
+percentageNonBTC
+
+#------------------------------------------------------------------------------#
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 #data <- read.csv("bitstampUSD_1-min_data_2012-01-01_to_2021-03-31.csv")
+
+
+
+
 
 str(data)
 timestamp <- data$Timestamp
